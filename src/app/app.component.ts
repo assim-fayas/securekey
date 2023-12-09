31c2d171464efacd10852a543bc70c279e007865
assim fayas <asim1fayas@gmail.com>
2023-12-09 21:03:09 +0530

    added text filds for input the password,copy the password,styled using bootstrap,integrated angular cdk clipboard service for copying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'securekey';
+  constructor(private clipboard: Clipboard) { }
+
+  passwordlength: number = 12
+  small: boolean = false
+  capital: boolean = false
+  number: boolean = false
+  special: boolean = false
+  passsmall: string = 'abcdefghijklmnopqrstuvwxyz'
+  passcapital: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  passnumber: string = "123456789"
+  passspecial: string = "!@#$%&*"
+  newpassword: string = ""
+
+  generate() {
+    let validcharacter = ''
+    if (this.small) {
+      validcharacter += this.passsmall
+    }
+    if (this.capital) {
+      validcharacter += this.passcapital
+    }
+    if (this.number) {
+      validcharacter += this.passnumber
+    }
+    if (this.special) {
+      validcharacter += this.passspecial
+    }
+    if (validcharacter.length === 0) {
+      alert("Please select any of the character type")
+    }
+    console.log(validcharacter);
+
+    let password = ""
+    for (let i = 0; i < this.passwordlength; i++) {
+      const randomIndex = Math.floor(Math.random() * validcharacter.length);
+      password += validcharacter.charAt(randomIndex)
+    }
+    this.newpassword = password
+  }
+  copy() {
+    const passinput = document.getElementById('passwordgenerated') as HTMLInputElement;
+    if (passinput) {
+      this.clipboard.copy(passinput.value);
+    }
+
+  }
 }
